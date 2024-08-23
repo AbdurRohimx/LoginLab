9f95aadd7cba6a17492f18b32270876e3ef82374
Rohim Bhai <108090636+AbdurRohimx@users.noreply.github.com>
2024-08-23 07:18:06 +0600

    Update login.php

--- login.php
+++ login.php
@@ -6,7 +6,7 @@ $pass = 'pass';
 
 if (!empty($_REQUEST) ) {
     if (!empty($_REQUEST[$user]) && !empty($_REQUEST[$pass])) {
-        if ($_REQUEST[$user] == 'testuser' && $_REQUEST[$pass] == 'testpass') {
+        if ($_REQUEST[$user] == 'ToxiCode' && $_REQUEST[$pass] == '1000') {
             $data = '1|'.md5($_REQUEST[$user] . $_REQUEST[$pass]).'|User: ' . $_REQUEST[$user] . ' successfully logged in!';
         }else{
             $data = '0|'.md5(date('Y-m-d h:i:s').rand(1, 9999)).'|Incorrect username or password!';
